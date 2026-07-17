211c236d7588df591c287dd967c16fd7a094c13e
jegor74 <j3g0r74@gmail.com>
2026-07-17 15:16:25 +0300

    feat: add .pdf loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,6 @@
-import re
 from pathlib import Path
+import re
+import pymupdf
 
 
 def load_text_files(raw_dir: str = "data/raw") -> dict:
@@ -14,11 +15,43 @@ def load_text_files(raw_dir: str = "data/raw") -> dict:
     if not raw_path.exists():
         raise FileNotFoundError(f"Directory {raw_dir} not found. Make it and put .txt files there.")
 
-    for file_path in raw_path.glob("*.txt"):                     # reading each file's path and opening it with UTF-8 encoding
+    for file_path in raw_path.glob("*.txt"):                       # reading each file's path and opening it with UTF-8 encoding
         with open(file_path, "r", encoding="utf-8") as file:
             text = file.read()
-        documents[file_path.name] = text                         # add file's text to dictionary
+        documents[file_path.name] = text                           # add file's text to dictionary
 
+    return documents
+
+
+def load_pdf_files(raw_dir: str = "data/raw") -> dict:
+    """
+    Loads all .pdf files from raw_dir directory.
+    Returns dictionary {file_name: text}.
+    """
+
+    documents = {}                 # dictionary
+    raw_path = Path(raw_dir)       # path with all .pdf files for loading
+
+    if not raw_path.exists():
+        raise FileNotFoundError(f"Directory {raw_dir} not found. Make it and put .pdf files there.")
+
+    for file_path in raw_path.glob("*.pdf"):                       # reading each file's path and opening it with UTF-8 encoding
+        try:
+            doc = pymupdf.open(file_path)                          # opening .pdf file
+        except Exception as e:                                     # if file can't be opened, raise exception and skip this file
+            print(f"⚠️ Skipping {file_path.name}: {e}")
+            continue
+
+        texts = []                                                 # empty array for text from .pdf file
+        for page in doc:                                           # getting text of .pdf file from each page
+            page_text = page.get_text()
+            if page_text != "":                                    # if page is empty, skip this page
+                texts.append(page_text) 
+        
+        text = "".join(texts)                                      # making text string from array
+        documents[file_path.name] = text                           # add file's text to dictionary
+        
+    documents = {k: v for k, v in documents.items() if v.strip()}  # removing empty documents from dictionary
     return documents
 
 
