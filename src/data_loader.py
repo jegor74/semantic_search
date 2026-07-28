1f313451dac9ff272739aa841f8ec25092799e9b
jegor74 <j3g0r74@gmail.com>
2026-07-28 14:08:21 +0300

    chore: add old .pdf loader function and update new .pdf loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,5 @@
 import re
+import pymupdf
 import pymupdf4llm
 from pathlib import Path
 
@@ -6,7 +7,9 @@ from pathlib import Path
 def load_text_files(raw_dir: str = "data/raw") -> dict:
     """
     Loads all .txt files from raw_dir directory.
-    Returns dictionary {file_name: text}.
+
+    Returns:
+    - dictionary {file_name: text}.
     """
 
     documents = {}                 # dictionary
@@ -16,6 +19,7 @@ def load_text_files(raw_dir: str = "data/raw") -> dict:
         raise FileNotFoundError(f"Directory {raw_dir} not found. Make it and put .txt files there.")
 
     for file_path in raw_path.glob("*.txt"):                       # reading each file's path and opening it with UTF-8 encoding
+        print(f"⏳ Processing (.txt): {file_path.name}")
         try:
             with open(file_path, "r", encoding="utf-8") as file:   # opening txt file
                 text = file.read()                                 # if file can't be opened, raise exception and skip this file
@@ -31,7 +35,9 @@ def load_text_files(raw_dir: str = "data/raw") -> dict:
 def load_pdf_files(raw_dir: str = "data/raw") -> dict:
     """
     Loads all .pdf files from raw_dir directory.
-    Returns dictionary {file_name: text}.
+
+    Returns:
+    - dictionary {file_name: text}.
     """
 
     documents = {}                 # dictionary
@@ -40,23 +46,58 @@ def load_pdf_files(raw_dir: str = "data/raw") -> dict:
     if not raw_path.exists():
         raise FileNotFoundError(f"Directory {raw_dir} not found. Make it and put .pdf files there.")
 
-    for file_path in raw_path.glob("*.pdf"):                                                      # reading each file's path and opening it with UTF-8 encoding
+    for file_path in raw_path.glob("*.pdf"):                       # reading each file's path and opening it with UTF-8 encoding
+        print(f"⏳ Processing (.pdf): {file_path.name}")
         try:
-            md_data = pymupdf4llm.to_markdown(file_path)                                          # making .md file from .pdf
+            md_data = pymupdf4llm.to_markdown(file_path)           # making .md file from .pdf
             
-            if isinstance(md_data, list):                                                         # taking text from list of dicts
+            if isinstance(md_data, list):                          # taking text from list of dicts
                 text = "\n".join([item.get("text", "") for item in md_data if "text" in item])
             else:
                 text = str(md_data)
 
-        except Exception as e:                                                                    # if file can't be opened, raise exception and skip this file
+        except Exception as e:                                     # if file can't be opened, raise exception and skip this file
             print(f"⚠️ Skipping {file_path.name}: {e}")
             continue
         
         if text.strip():
             documents[file_path.name] = text
 
-    documents = {k: v for k, v in documents.items() if v.strip()}                                 # removing empty documents from dictionary
+    documents = {k: v for k, v in documents.items() if v.strip()}  # removing empty documents from dictionary
+    return documents
+
+
+def load_pdf_files_old(raw_dir: str = "data/raw") -> dict:
+    """
+    Loads all .pdf files from raw_dir directory.
+
+    Returns:
+    - dictionary {file_name: text}.
+    """
+
+    documents = {}                 # dictionary
+    raw_path = Path(raw_dir)       # path with all .pdf files for loading
+
+    if not raw_path.exists():
+        raise FileNotFoundError(f"Directory {raw_dir} not found. Make it and put .pdf files there.")
+
+    for file_path in raw_path.glob("*.pdf"):                       # reading each file's path and opening it with UTF-8 encoding
+        try:
+            doc = pymupdf.open(file_path)                          # opening .pdf file
+        except Exception as e:                                     # if file can't be opened, raise exception and skip this file
+            print(f"⚠️ Skipping {file_path.name}: {e}")
+            continue
+
+        texts = []                                                 # empty array for text from .pdf file
+        for page in doc:                                           # getting text of .pdf file from each page
+            page_text = page.get_text()
+            if page_text != "":                                    # if page is empty, skip this page
+                texts.append(page_text) 
+        
+        text = "".join(texts)                                      # making text string from array
+        documents[file_path.name] = text                           # add file's text to dictionary
+        
+    documents = {k: v for k, v in documents.items() if v.strip()}  # removing empty documents from dictionary
     return documents
 
 
